5e8891cebb05e31586f0a0a0ec8a8becf72b7ad7
Timothy Standen <timothyf1@live.co.uk>
2022-12-22 12:01:21 +0000

    Change module list variable and added module desciption to module page

--- portfolio/routes/module.py
+++ portfolio/routes/module.py
@@ -3,7 +3,7 @@ from portfolio import app
 from portfolio.models import Module, Post
 
 @app.route("/module")
-def module():
+def module_list():
     modules = Module.query.all()
     return render_template('module-list.html',title='Modules', modules=modules)
 
@@ -12,4 +12,4 @@ def module_page(name):
     module = Module.query.filter_by(name=name).first_or_404()
     print(module.id)
     posts = Post.query.filter_by(module_id=module.id)
-    return render_template('module-posts.html', title='name', module=name, posts=posts)
+    return render_template('module-posts.html', title='name', module=module, posts=posts)
