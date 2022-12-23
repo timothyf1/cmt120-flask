15514085f535a5f3b89406dbdba56033b642a230
Timothy Standen <timothyf1@live.co.uk>
2022-12-23 11:47:39 +0000

    Created blueprints for home and module - Moved routing to the new blueprints - Updated template locations for each blueprint

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -4,10 +4,11 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '<please generate a new secret key>'
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://%s:%s@csmysql.cs.cf.ac.uk:3306/%s' % (os.environ["mysql_username"], os.environ["mysql_password"], os.environ["mysql_db_name"])
-
 db = SQLAlchemy(app)
 
-from portfolio.routes import main, module
+from portfolio.home.home import home_bp
+from portfolio.modules.modules import modules_bp
 
+app.register_blueprint(home_bp)
+app.register_blueprint(modules_bp)
