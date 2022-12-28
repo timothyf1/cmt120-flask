2e7c907c629bc53075aabeadfee92ac57315e61d
Timothy Standen <timothyf1@live.co.uk>
2022-12-28 15:51:23 +0000

    Updated models to split modules into courses

--- portfolio/models.py
+++ portfolio/models.py
@@ -7,7 +7,7 @@ from portfolio import db, app
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(10), nullable=False)
+    username = db.Column(db.String(10), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
     created_on = db.Column(db.DateTime, default=datetime.utcnow)
     last_login = db.Column(db.DateTime)
@@ -20,9 +20,16 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password, password)
 
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    location = db.Column(db.String(40), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(200), nullable=False)
 
 class Module(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
     name = db.Column(db.String(20), nullable=False)
     description = db.Column(db.String(200))
 
