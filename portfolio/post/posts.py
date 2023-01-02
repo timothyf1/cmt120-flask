648709773a7c41d6c718fa102dc355ca7a749f73
Timothy Standen <timothyf1@live.co.uk>
2023-01-02 15:30:58 +0000

    Phasing markdown in posts from DB

--- portfolio/post/posts.py
+++ portfolio/post/posts.py
@@ -3,6 +3,8 @@ from flask_login import login_required
 from .. import db
 from ..models import Course, Module, Post
 
+import markdown
+
 bp_posts = Blueprint('bp_posts', __name__, template_folder='templates')
 
 @bp_posts.route("/")
@@ -13,4 +15,6 @@ def posts_list():
 @bp_posts.route("/<string:title>")
 def view_post(title):
     post = Post.query.filter_by(title=title).first_or_404()
-    return render_template('post-view.html', title=post.title, post=post)
+    content = markdown.markdown(post.content)
+    print(content)
+    return render_template('post-view.html', title=post.title, post=post, content=content)
