da1ea553f8b3894d06993626e427b7b2abe98cad
Timothy Standen <timothyf1@live.co.uk>
2023-01-02 10:29:08 +0000

    New form to create a course

--- portfolio/course/course.py
+++ portfolio/course/course.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, redirect
+from flask_login import login_required
+from .. import db
 from ..models import Course, Module
+
+from .form import New_Course
 
 bp_education = Blueprint('bp_education', __name__, template_folder='templates')
 
@@ -14,3 +18,20 @@ def course_page(name):
     # print(module.id)
     modules = Module.query.filter_by(course_id=course.id)
     return render_template('course-modules.html', title=name, course=course, modules=modules)
+
+@bp_education.route("/new-course", methods=['GET', 'POST'])
+@login_required
+def new_course():
+    form = New_Course()
+    if form.validate_on_submit():
+        course = Course(
+            name = form.name.data,
+            location = form.location.data,
+            year = int(form.year.data),
+            description = form.description.data
+            )
+        db.session.add(course)
+        db.session.commit()
+        return redirect(url_for('bp_education.course_page', name=course.name))
+
+    return render_template('new-course.html', title='Add a course', form=form)
---
+++ portfolio/course/form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class New_Course(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    location = StringField('Location', validators=[DataRequired()])
+    year = StringField('Year', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Add Course')
