7787634e398c2d1d3bbe3b20ab335adfb603a1f0
Timothy Standen <timothyf1@live.co.uk>
2023-01-06 12:37:48 +0000

    Moved profile into own blueprint from auth

--- portfolio/auth/auth.py
+++ portfolio/auth/auth.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 from flask import Blueprint, render_template, request, url_for, redirect, flash
 from flask_login import login_required, logout_user, current_user, login_user
-from .. import db, login_manager
 from ..models import User
-from .forms import Login_form, Signup_form, Change_password, Dark_mode
+from .forms import Login_form, Signup_form
 
 bp_auth = Blueprint('bp_auth', __name__, template_folder='templates', static_folder='static')
 
@@ -22,7 +21,6 @@ def login():
         flash('Invalid username and password combination')
         # return redirect(url_for('bp_auth.login'))
     return render_template('login.html', title='Login', form=form)
-
 
 @bp_auth.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -52,39 +50,3 @@ def logout():
     logout_user()
     flash('Logout Successful')
     return redirect(url_for('bp_home.home'))
-
-@bp_auth.route('/changepassword', methods=['GET', 'POST'])
-@login_required
-def change_password():
-    form = Change_password()
-    if form.validate_on_submit():
-        if current_user.verify_password(form.current_password.data):
-            current_user.set_password(form.new_password.data)
-            db.session.commit()
-            return redirect(url_for('bp_home.home'))
-        form.current_password.errors = ['Password Incorrect']
-    return render_template('changepassword.html', title='Change Password', form=form)
-
-@bp_auth.route('/darkmode', methods=['GET', 'POST'])
-@login_required
-def dark_mode_setting():
-    form = Dark_mode()
-    if form.validate_on_submit():
-        current_user.dark_mode = form.pref.data
-        db.session.commit()
-        return redirect(url_for('bp_auth.user_profile'))
-    form.pref.default = current_user.dark_mode
-    form.process()
-    return render_template('darkmode.html', title='Change Dark Mode Setting', form=form)
-
-@bp_auth.route('/profile')
-@login_required
-def user_profile():
-    return render_template('profile.html', title='Profile')
-
-@login_manager.user_loader
-def load_user(user_id):
-    if user_id is None:
-        return None
-    return User.query.get(int(user_id))
-
--- portfolio/auth/forms.py
+++ portfolio/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, RadioField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo, Length
 
 class Signup_form(FlaskForm):
@@ -12,13 +12,3 @@ class Login_form(FlaskForm):
     username = StringField('Username', validators=[DataRequired(message="Please enter a username")])
     password = PasswordField('Password', validators=[DataRequired(message="Please enter a password")])
     submit = SubmitField('Log In')
-
-class Change_password(FlaskForm):
-    current_password = PasswordField('Current Password', validators=[DataRequired()])
-    new_password = PasswordField('New Password', validators=[DataRequired(), Length(min=8, message="Passwords must be atleast 8 characters")])
-    confirm_new_password = PasswordField('Confirm New Password', validators=[DataRequired(), EqualTo('new_password', message="Passwords do not match")])
-    submit = SubmitField('Change Password')
-
-class Dark_mode(FlaskForm):
-    submit = SubmitField('Apply')
-    pref = RadioField('Select dark mode preference', choices=[(0, 'System'), (1, 'Light'), (2, 'Dark')], default=0)
