b7b49e96e380ebcb4c8d75457eb275281a79b1ac
Timothy Standen <timothyf1@live.co.uk>
2023-01-06 12:56:46 +0000

    moved styling around

--- portfolio/profile/profile.py
+++ portfolio/profile/profile.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for, redirect
+from flask import Blueprint, render_template, url_for, redirect, flash
 from flask_login import login_required, current_user
 from .. import db
 from ..models import User
