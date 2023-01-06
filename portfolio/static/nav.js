48c563df18a2a55c4afbbfb6ff22ac6f7bd9e3fd
Timothy Standen <timothyf1@live.co.uk>
2023-01-06 12:13:31 +0000

    nav bar changes - resized to content width - padding above - sticks to top of screen on scroll

---
+++ portfolio/static/nav.js
@@ -0,0 +1,23 @@
+// JS function to add and remove class
+// This code was adapted from w3schools - How TO - Sticky/Affix Navbar
+// accessed on 2023-01-05
+// https://www.w3schools.com/HOWTO/howto_js_navbar_sticky.asp
+
+let header = document.getElementsByTagName("header")[0];
+let headerStyle = getComputedStyle(header)
+let resetHeight = headerStyle.marginTop.replace(/[^0-9]/g, '');
+let stickyHeight = header.offsetTop;
+let message = document.getElementsByClassName("messages")[0];
+
+window.onscroll = () => {
+    if (window.pageYOffset > stickyHeight) {
+        header.classList.add("sticky")
+        message.classList.add("sticky-pad")
+    }
+    if (window.pageYOffset <= resetHeight) {
+        header.classList.remove("sticky");
+        message.classList.remove("sticky-pad");
+    }
+}
+
+/* end of referenced code */
