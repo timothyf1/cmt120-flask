ecabe05e47701d6f60946e21918090a5c0b93c19
Timothy Standen <timothyf1@live.co.uk>
2023-01-08 22:13:16 +0000

    Set current darkmode setting on form

--- portfolio/profile/static/set-darkmode.js
+++ portfolio/profile/static/set-darkmode.js
@@ -1,7 +1,13 @@
-let darkmodeField = document.getElementById('darkmode');
-
 function setDarkMode(value) {
     localStorage.setItem('darkmode', value);
     console.log(localStorage.getItem('darkmode'))
     setCSSColourFile(`${value}`)
 }
+
+function setCurrentDarkModeRadio() {
+    let currentSetting = readDarkModeSetting()
+    let currentRadio = document.getElementById(`dark-${currentSetting}`)
+    currentRadio.checked = true
+}
+
+setCurrentDarkModeRadio()
