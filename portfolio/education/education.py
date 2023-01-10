beffe51da39845ef84cf23df4054892841862d62
Timothy Standen <timothyf1@live.co.uk>
2023-01-10 10:29:20 +0000

    missing import

--- portfolio/education/education.py
+++ portfolio/education/education.py
@@ -2,7 +2,7 @@ import markdown
 import bleach
 
 from flask import Blueprint, render_template, url_for, redirect, request
-from flask_login import login_required
+from flask_login import login_required, current_user
 from flask_breadcrumbs import register_breadcrumb
 
 from .. import db
