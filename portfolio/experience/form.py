68aa15dc1bf831c42e6583ce719919eb69ea2a6c
Timothy Standen <timothyf1@live.co.uk>
2023-01-14 15:54:59 +0000

    updated validation on experience

--- portfolio/experience/form.py
+++ portfolio/experience/form.py
@@ -1,13 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, IntegerField, DateField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
+
+def check_start_before_end(form, field):
+    if not form.current.data:
+        if form.end.data < form.start.data:
+            raise ValidationError('End date must be after start date')
 
 class Edit_Experience(FlaskForm):
-    title = StringField('Job Title', validators=[DataRequired()])
-    employer = StringField('Employer', validators=[DataRequired()])
-    location = StringField('Location', validators=[DataRequired()])
+    title = StringField('Job Title', validators=[DataRequired(), Length(max=50)])
+    employer = StringField('Employer', validators=[DataRequired(), Length(max=50)])
+    location = StringField('Location', validators=[DataRequired(), Length(max=50)])
     start = DateField('Start Date', validators=[DataRequired()])
-    end = DateField('End Date')
+    end = DateField('End Date', validators=[check_start_before_end])
     current = BooleanField('Current Job')
     description = StringField('Description')
     submit = SubmitField('Add Experience')
