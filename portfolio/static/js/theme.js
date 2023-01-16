4181f068210094dedf3b2f121f7440de03aa62e5
Timothy Standen <timothyf1@live.co.uk>
2023-01-16 13:00:23 +0000

    updated js and html for access mode

--- portfolio/static/js/theme.js
+++ portfolio/static/js/theme.js
@@ -1,4 +1,4 @@
-function setCSSColourFile(darkmode) {
+function setCSSColourDarkMode(darkmode) {
     let cssThemeTag = document.getElementById('theme');
     switch (darkmode) {
         case '0':
@@ -13,14 +13,36 @@ function setCSSColourFile(darkmode) {
     }
 }
 
-function readDarkModeSetting() {
-    if (localStorage.getItem('darkmode')) {
-        return localStorage.getItem('darkmode');
+function setCSSAccessMode(access) {
+    let cssThemeTag = document.getElementById('theme');
+    if (access === '0') {
+        cssThemeTag.href = cssThemeTag.href.replace("-access.css", ".css");
     } else {
-        localStorage.setItem('darkmode', '0');
-        return '0';
+        cssThemeTag.href = cssThemeTag.href.replace(".css", "-access.css");
     }
 }
 
-let darkmode = readDarkModeSetting();
-setCSSColourFile(darkmode);
+function readDisplaySettings() {
+    let dark;
+    let access;
+    if (localStorage.getItem('darkmode')) {
+        dark = localStorage.getItem('darkmode');
+    } else {
+        localStorage.setItem('darkmode', '0');
+        dark = '0';
+    }
+    if (localStorage.getItem('access')) {
+        access = localStorage.getItem('access');
+    } else {
+        localStorage.setItem('access', '0');
+        access = '0';
+    }
+    return {
+        'darkmode' : dark,
+        'access' : access
+    }
+}
+
+let themeset = readDisplaySettings();
+setCSSColourDarkMode(themeset['darkmode']);
+setCSSAccessMode(themeset['access'])
