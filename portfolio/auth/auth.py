232fedd6420032596d39d45c8cf8f5417e5f1595
Timothy Standen <timothyf1@live.co.uk>
2023-01-17 11:12:35 +0000

    removing signup page code

--- portfolio/auth/auth.py
+++ portfolio/auth/auth.py
@@ -5,7 +5,7 @@ from flask_login import login_required, logout_user, current_user, login_user
 
 from .. import db
 from ..models import User
-from .forms import Login_form, Signup_form, New_password
+from .forms import Login_form, New_password
 
 bp_auth = Blueprint('bp_auth', __name__, template_folder='templates', static_folder='static')
 
@@ -39,27 +39,6 @@ def new_password():
         flash('Password updated', 'info')
         return redirect(url_for('bp_home.home'))
     return render_template('newpassword.html', form=form)
-
-@bp_auth.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if current_user.is_authenticated:
-        return redirect(url_for('bp_home.home'))
-    form = Signup_form()
-    if form.validate_on_submit():
-        existing_user = User.query.filter_by(username = form.username.data).first()
-
-        if existing_user is None:
-            user = User(username = form.username.data)
-            user.set_password(form.password.data)
-
-            db.session.add(user)
-            db.session.commit()
-            login_user(user)
-            current_user.last_login = datetime.utcnow
-            db.session.commit()
-            return redirect(url_for('bp_home.home'))
-        flash('Username already used', 'error')
-    return render_template('signup.html', title='Signup', form=form)
 
 @bp_auth.route('/logout')
 @login_required
